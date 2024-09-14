583f5fa8366a04ba7bcedaf22fe64c8863ba44de
GersonAparecidoLima <gersonaparecido@gmail.com>
2024-09-14 13:21:36 -0300

    Inicio desafio

---
+++ src/Bill.java
@@ -0,0 +1,12 @@
+
+public class Bill {
+
+	public Bill() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		System.out.println("Olá Jesus pode te salvar!!!");
+	}
+
+}
